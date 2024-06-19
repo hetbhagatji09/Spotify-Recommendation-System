644757610e6344ab178ade21cc4199c2709b64dd
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-06-19 15:08:14 +0530

    Sample Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+df=pd.read_csv('spotify.csv')
+df=df.iloc[0:20000,:]
+
+def Recommened(artist):
+    songs=df[df['artist']==artist]['title'].values
+    return songs
+st.title('Spotify Data Analysis')
+
+artists=df['artist'].unique()
+artist=st.selectbox('spotify',(artists))
+
+if st.button("Recommened"):
+    songs=Recommened(artist)
+    for i in songs:
+        st.text(i)
